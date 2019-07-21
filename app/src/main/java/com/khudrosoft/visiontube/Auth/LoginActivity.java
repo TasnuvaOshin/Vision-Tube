11e23ffc52b0d364b957d10b3d4f4997c7f59ef7
Tasnuva Tabassum Oshin <tasnuva.oshin12@gmail.com>
2019-07-21 16:30:05 +0600

    Vision Tube New Release

--- app/src/main/java/com/khudrosoft/visiontube/Auth/LoginActivity.java
+++ app/src/main/java/com/khudrosoft/visiontube/Auth/LoginActivity.java
@@ -7,6 +7,7 @@ import android.annotation.TargetApi;
 import android.content.Context;
 import android.graphics.Color;
 import android.graphics.SurfaceTexture;
+import android.media.MediaPlayer;
 import android.net.ConnectivityManager;
 import android.net.Uri;
 import android.os.Build;
@@ -16,6 +17,7 @@ import android.view.View;
 import android.webkit.WebChromeClient;
 import android.webkit.WebResourceError;
 import android.webkit.WebResourceRequest;
+import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 import android.widget.ImageView;
@@ -28,7 +30,7 @@ public class LoginActivity extends AppCompatActivity {
     public WebView webView;
     public ImageView imageView;
     private VideoView videoView;
-    @SuppressLint("SetJavaScriptEnabled")
+    @SuppressLint({"SetJavaScriptEnabled", "NewApi"})
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -42,19 +44,39 @@ public class LoginActivity extends AppCompatActivity {
         String path = "android.resource://" + getPackageName() + "/" + R.raw.vv;
         videoView.setVideoURI(Uri.parse(path));
         videoView.start();
+
         imageView.setVisibility(View.GONE);
         webView.setVisibility(View.GONE);
-        webView.getSettings().setJavaScriptEnabled(true);
         webView.setWebChromeClient(new WebChromeClient());
-        webView.clearCache(true);
-        webView.clearHistory();
-        webView.invalidate();
+        webView.getSettings().setLoadsImagesAutomatically(true);
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.getSettings().setAllowFileAccess(true);
+        webView.setScrollBarStyle(View.SCROLLBARS_INSIDE_OVERLAY);
+        webView.getSettings().setJavaScriptCanOpenWindowsAutomatically(true);
+        webView.getSettings().setPluginState(WebSettings.PluginState.ON);
+        webView.getSettings().setMediaPlaybackRequiresUserGesture(false);
+        webView.loadUrl("https://www.w3schools.com/html/html5_video.asp");
+
+
+
+
+        videoView.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+
+            @Override
+            public void onCompletion(MediaPlayer mp) {
+               videoView.setVisibility(View.GONE);
+            }
+        });
+
+
 
         webView.setWebChromeClient(new WebChromeClient() {
             public void onProgressChanged(WebView view, int progress) {
                 if (progress == 100) {
                     imageView.setVisibility(View.GONE);
+                    videoView.setVisibility(View.GONE);
                     webView.setVisibility(View.VISIBLE);
+
 
                 }
             }
@@ -62,19 +84,18 @@ public class LoginActivity extends AppCompatActivity {
 
         });
         webView.setWebViewClient(webClient);
-        webView.getSettings().setJavaScriptCanOpenWindowsAutomatically(true);
-        webView.setLayerType(View.LAYER_TYPE_SOFTWARE, null);
+//        webView.getSettings().setJavaScriptCanOpenWindowsAutomatically(true);
+//        webView.setLayerType(View.LAYER_TYPE_SOFTWARE, null);
         webView.setBackgroundColor(Color.TRANSPARENT);
 
         if(isNetworkConnected()) {
             webView.loadUrl("http://v-tube.xyz/login");
-
+          //  videoView.setVisibility(View.GONE);
         }else{
             imageView.setImageResource(R.drawable.nointernet);
             imageView.setVisibility(View.VISIBLE);
             webView.setVisibility(View.GONE);
             Toast.makeText(this, "No Internet Connection", Toast.LENGTH_SHORT).show();
-
         }
     }
 
@@ -123,4 +144,6 @@ public class LoginActivity extends AppCompatActivity {
 
         }
     }
+
+
 }
