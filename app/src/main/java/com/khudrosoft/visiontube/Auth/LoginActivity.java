982f2d0be09bb2609d4681fa1da11f5e5d0c1417
Tasnuva Tabassum Oshin <tasnuva.oshin12@gmail.com>
2019-12-16 12:43:17 +0600

    Webview download and upload system

--- app/src/main/java/com/khudrosoft/visiontube/Auth/LoginActivity.java
+++ app/src/main/java/com/khudrosoft/visiontube/Auth/LoginActivity.java
@@ -1,42 +1,63 @@
 package com.khudrosoft.visiontube.Auth;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
 import android.annotation.SuppressLint;
 import android.annotation.TargetApi;
+import android.app.AlertDialog;
+import android.app.DownloadManager;
+import android.content.ActivityNotFoundException;
 import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.graphics.Color;
-import android.graphics.SurfaceTexture;
 import android.media.MediaPlayer;
 import android.net.ConnectivityManager;
 import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
-import android.view.TextureView;
+import android.os.Environment;
 import android.view.View;
+import android.webkit.DownloadListener;
+import android.webkit.ValueCallback;
 import android.webkit.WebChromeClient;
 import android.webkit.WebResourceError;
 import android.webkit.WebResourceRequest;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
+import android.widget.FrameLayout;
 import android.widget.ImageView;
 import android.widget.Toast;
 import android.widget.VideoView;
 
 import com.khudrosoft.visiontube.R;
+import com.khudrosoft.visiontube.Update.UpdateHelper;
 
-public class LoginActivity extends AppCompatActivity {
+import java.util.Random;
+
+public class LoginActivity extends AppCompatActivity implements UpdateHelper.onUpdateCheckListener {
+    private static final int MY_PERMISSIONS_REQUEST_LOCATION = 99;
     public WebView webView;
     public ImageView imageView;
     private VideoView videoView;
+    private ValueCallback<Uri> mUploadMessage;
+    public ValueCallback<Uri[]> uploadMessage;
+    public static final int REQUEST_SELECT_FILE = 100;
+    private final static int FILECHOOSER_RESULTCODE = 1;
+
     @SuppressLint({"SetJavaScriptEnabled", "NewApi"})
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        UpdateHelper.with(this).onUpdateCheck(this).check();
         webView = findViewById(R.id.webview);
-        imageView  = findViewById(R.id.img);
+        imageView = findViewById(R.id.img);
         videoView = findViewById(R.id.videoview);
         videoView.setZOrderOnTop(true);
 
@@ -54,8 +75,12 @@ public class LoginActivity extends AppCompatActivity {
         webView.setScrollBarStyle(View.SCROLLBARS_INSIDE_OVERLAY);
         webView.getSettings().setJavaScriptCanOpenWindowsAutomatically(true);
         webView.getSettings().setPluginState(WebSettings.PluginState.ON);
-        webView.getSettings().setMediaPlaybackRequiresUserGesture(false);
+        //webView.getSettings().setMediaPlaybackRequiresUserGesture(true);
         webView.loadUrl("https://www.w3schools.com/html/html5_video.asp");
+        webView.getSettings().setAllowFileAccess(true);
+        webView.getSettings().setAllowFileAccess(true);
+        webView.getSettings().setAllowContentAccess(true);
+        
 
 
 
@@ -64,14 +89,18 @@ public class LoginActivity extends AppCompatActivity {
 
             @Override
             public void onCompletion(MediaPlayer mp) {
-               videoView.setVisibility(View.GONE);
+                videoView.setVisibility(View.GONE);
             }
         });
 
 
-
         webView.setWebChromeClient(new WebChromeClient() {
+
+
+
+
             public void onProgressChanged(WebView view, int progress) {
+
                 if (progress == 100) {
                     imageView.setVisibility(View.GONE);
                     videoView.setVisibility(View.GONE);
@@ -81,17 +110,88 @@ public class LoginActivity extends AppCompatActivity {
                 }
             }
 
+            // For 3.0+ Devices (Start)
+            // onActivityResult attached before constructor
+            protected void openFileChooser(ValueCallback uploadMsg, String acceptType) {
+                mUploadMessage = uploadMsg;
+                Intent i = new Intent(Intent.ACTION_GET_CONTENT);
+                i.addCategory(Intent.CATEGORY_OPENABLE);
+                i.setType("image/*");
+                startActivityForResult(Intent.createChooser(i, "File Browser"), FILECHOOSER_RESULTCODE);
+            }
 
+
+            // For Lollipop 5.0+ Devices
+            public boolean onShowFileChooser(WebView mWebView, ValueCallback<Uri[]> filePathCallback, WebChromeClient.FileChooserParams fileChooserParams) {
+                if (uploadMessage != null) {
+                    uploadMessage.onReceiveValue(null);
+                    uploadMessage = null;
+                }
+
+                uploadMessage = filePathCallback;
+
+                Intent intent = fileChooserParams.createIntent();
+                try {
+                    startActivityForResult(intent, REQUEST_SELECT_FILE);
+                } catch (ActivityNotFoundException e) {
+                    uploadMessage = null;
+                    Toast.makeText(LoginActivity.this, "Cannot Open File Chooser", Toast.LENGTH_LONG).show();
+                    return false;
+                }
+                return true;
+            }
+
+            //For Android 4.1 only
+            protected void openFileChooser(ValueCallback<Uri> uploadMsg, String acceptType, String capture) {
+                mUploadMessage = uploadMsg;
+                Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
+                intent.addCategory(Intent.CATEGORY_OPENABLE);
+                intent.setType("image/*");
+                startActivityForResult(Intent.createChooser(intent, "File Browser"), FILECHOOSER_RESULTCODE);
+            }
+
+            protected void openFileChooser(ValueCallback<Uri> uploadMsg) {
+                mUploadMessage = uploadMsg;
+                Intent i = new Intent(Intent.ACTION_GET_CONTENT);
+                i.addCategory(Intent.CATEGORY_OPENABLE);
+                i.setType("image/*");
+                startActivityForResult(Intent.createChooser(i, "File Chooser"), FILECHOOSER_RESULTCODE);
+            }
+
+
+        });
+
+
+        webView.setDownloadListener(new DownloadListener() {
+
+            @Override
+            public void onDownloadStart(String url, String userAgent,
+                                        String contentDisposition, String mimetype,
+                                        long contentLength) {
+                if (checkLocationPermission()) {
+                    DownloadManager.Request request = new DownloadManager.Request(
+                            Uri.parse(url));
+
+                    request.allowScanningByMediaScanner();
+                    request.setNotificationVisibility(DownloadManager.Request.VISIBILITY_VISIBLE_NOTIFY_COMPLETED); //Notify client once download is completed!
+                    request.setDestinationInExternalPublicDir(Environment.DIRECTORY_DOWNLOADS, random());
+                    DownloadManager dm = (DownloadManager) getSystemService(DOWNLOAD_SERVICE);
+                    dm.enqueue(request);
+                    Toast.makeText(getApplicationContext(), "Downloading File", //To notify the Client that the file is being downloaded
+                            Toast.LENGTH_LONG).show();
+                }
+
+            }
         });
         webView.setWebViewClient(webClient);
 //        webView.getSettings().setJavaScriptCanOpenWindowsAutomatically(true);
 //        webView.setLayerType(View.LAYER_TYPE_SOFTWARE, null);
         webView.setBackgroundColor(Color.TRANSPARENT);
 
-        if(isNetworkConnected()) {
+        if (isNetworkConnected()) {
             webView.loadUrl("http://v-tube.xyz/login");
-          //  videoView.setVisibility(View.GONE);
-        }else{
+            //  videoView.setVisibility(View.GONE);
+        } else {
             imageView.setImageResource(R.drawable.nointernet);
             imageView.setVisibility(View.VISIBLE);
             webView.setVisibility(View.GONE);
@@ -115,6 +215,21 @@ public class LoginActivity extends AppCompatActivity {
         return cm.getActiveNetworkInfo() != null;
     }
 
+    @Override
+    public void onUpdateCheckListener(String urlApp) {
+        androidx.appcompat.app.AlertDialog alertDialog = new androidx.appcompat.app.AlertDialog.Builder(this).setTitle("Update Your App").setMessage("Please Update Your App to new Version.").setPositiveButton("UPDATE", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                final String appPackageName = getPackageName(); // getPackageName() from Context or Activity object
+                startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("https://play.google.com/store/apps/details?id=" + appPackageName)));
+
+
+            }
+        }).create();
+
+        alertDialog.show();
+    }
+
 
     public class WebClient extends WebViewClient {
 
@@ -122,7 +237,8 @@ public class LoginActivity extends AppCompatActivity {
         @TargetApi(Build.VERSION_CODES.M)
         public void onReceivedError(WebView view, WebResourceRequest request, WebResourceError error) {
             super.onReceivedError(view, request, error);
-            webView.loadUrl("file:///android_asset/error.html");      }
+            webView.loadUrl("file:///android_asset/error.html");
+        }
 
         @SuppressWarnings("deprecation")
         public void onReceivedError(WebView view, int errorCode, String description, String failingUrl) {
@@ -141,9 +257,87 @@ public class LoginActivity extends AppCompatActivity {
             return false;
 
 
+        }
+    }
+
+    public boolean checkLocationPermission() {
+        if (ContextCompat.checkSelfPermission(LoginActivity.this,
+                Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                != PackageManager.PERMISSION_GRANTED) {
+
+            // Should we show an explanation?
+            if (ActivityCompat.shouldShowRequestPermissionRationale(LoginActivity.this,
+                    Manifest.permission.WRITE_EXTERNAL_STORAGE)) {
+
+                // Show an explanation to the user *asynchronously* -- don't block
+                // this thread waiting for the user's response! After the user
+                // sees the explanation, try again to request the permission.
+                new AlertDialog.Builder(LoginActivity.this)
+                        .setTitle("Permission")
+                        .setMessage("Please Share/on Your Location")
+                        .setPositiveButton("ok", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialogInterface, int i) {
+                                //Prompt the user once explanation has been shown
+                                ActivityCompat.requestPermissions(LoginActivity.this,
+                                        new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE},
+                                        MY_PERMISSIONS_REQUEST_LOCATION);
+                                startActivity(new Intent(LoginActivity.this, LoginActivity.class));
+                                LoginActivity.this.overridePendingTransition(0, 0);
+                            }
+                        })
+                        .create()
+                        .show();
+
+
+            } else {
+                // No explanation needed, we can request the permission.
+                ActivityCompat.requestPermissions(LoginActivity.this,
+                        new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE},
+                        MY_PERMISSIONS_REQUEST_LOCATION);
+
+            }
+
+            return false;
+        } else {
+
+            return true;
 
         }
     }
 
+    public static String random() {
+        Random generator = new Random();
+        StringBuilder randomStringBuilder = new StringBuilder();
+        int randomLength = generator.nextInt(5);
+        char tempChar;
+        for (int i = 0; i < randomLength; i++) {
+            tempChar = (char) (generator.nextInt(96) + 32);
+            randomStringBuilder.append(tempChar);
+        }
+        return randomStringBuilder.toString();
+    }
+
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent intent) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            if (requestCode == REQUEST_SELECT_FILE) {
+                if (uploadMessage == null)
+                    return;
+                uploadMessage.onReceiveValue(WebChromeClient.FileChooserParams.parseResult(resultCode, intent));
+                uploadMessage = null;
+            }
+        } else if (requestCode == FILECHOOSER_RESULTCODE) {
+            if (null == mUploadMessage)
+                return;
+            // Use MainActivity.RESULT_OK if you're implementing WebView inside Fragment
+            // Use RESULT_OK only if you're implementing WebView inside an Activity
+            Uri result = intent == null || resultCode != LoginActivity.RESULT_OK ? null : intent.getData();
+            mUploadMessage.onReceiveValue(result);
+            mUploadMessage = null;
+        } else
+            Toast.makeText(LoginActivity.this, "Failed to Upload Image", Toast.LENGTH_LONG).show();
+    }
 
 }
